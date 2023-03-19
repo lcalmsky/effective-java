62cfe9f0b1ba7421cdeddcdf616804f24591164d
Jaime <lcalmsky@gmail.com>
2023-03-20 00:57:02 +0900

    정적 팩터리 메서드의 장점1

---
+++ src/main/java/io/lcalmsky/effectivejava/chapter01/item01/Order.java
@@ -0,0 +1,22 @@
+package io.lcalmsky.effectivejava.chapter01.item01;
+
+public class Order {
+
+  private boolean prime;
+  private boolean urgent;
+  private Product product;
+
+  public static Order primeOrder(Product product, boolean prime) {
+    Order order = new Order();
+    order.prime = true;
+    order.product = product;
+    return order;
+  }
+
+  public static Order urgentOrder(Product product, boolean prime) {
+    Order order = new Order();
+    order.urgent = true;
+    order.product = product;
+    return order;
+  }
+}
