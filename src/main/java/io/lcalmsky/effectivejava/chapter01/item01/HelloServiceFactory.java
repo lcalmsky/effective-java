2dad02ed1c94c67ad25141cad152a2072d8be362
Jaime <lcalmsky@gmail.com>
2023-03-21 23:45:34 +0900

    리플렉션

--- src/main/java/io/lcalmsky/effectivejava/chapter01/item01/HelloServiceFactory.java
+++ src/main/java/io/lcalmsky/effectivejava/chapter01/item01/HelloServiceFactory.java
@@ -1,5 +1,15 @@
 package io.lcalmsky.effectivejava.chapter01.item01;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class HelloServiceFactory {
 
+  public static void main(String[] args)
+      throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    Class<?> aClass = Class.forName("io.lcalmsky.effectivejava.chapter01.item01.KoreanHelloService");
+    Constructor<?> constructor = aClass.getConstructor();
+    HelloService helloService = (HelloService) constructor.newInstance();
+    System.out.println(helloService.hello());
+  }
 }
--- src/main/java/io/lcalmsky/effectivejava/chapter01/item01/KoreanHelloService.java
+++ src/main/java/io/lcalmsky/effectivejava/chapter01/item01/KoreanHelloService.java
@@ -1,5 +1,9 @@
 package io.lcalmsky.effectivejava.chapter01.item01;
 
-public class KoreanHelloService {
+public class KoreanHelloService implements HelloService{
 
+  @Override
+  public String hello() {
+    return "안녕";
+  }
 }
