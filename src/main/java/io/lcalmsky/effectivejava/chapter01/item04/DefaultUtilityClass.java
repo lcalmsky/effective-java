a59233bd29741967a7e3b522c37232d85de5d254
Jaime <lcalmsky@gmail.com>
2023-03-31 03:20:10 +0900

    feat: 아이템 4 핵심정리

---
+++ src/main/java/io/lcalmsky/effectivejava/chapter01/item04/DefaultUtilityClass.java
@@ -0,0 +1,9 @@
+//package io.lcalmsky.effectivejava.chapter01.item04;
+//
+//public class DefaultUtilityClass extends UtilityClass {
+//
+//  public static void main(String[] args) {
+//    DefaultUtilityClass utilityClass = new DefaultUtilityClass();
+//  }
+//
+//}
