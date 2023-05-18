fb852cada07bcbdee58f8dd71f281207579bfba5
Jaime <lcalmsky@gmail.com>
2023-05-19 00:12:39 +0900

    아이템 10. 핵심 정리 3 - equals 규약 - 추이성

--- src/main/java/io/lcalmsky/effectivejava/chapter01/item10/ColorPoint.java
+++ src/main/java/io/lcalmsky/effectivejava/chapter01/item10/ColorPoint.java
@@ -1,5 +1,7 @@
 package io.lcalmsky.effectivejava.chapter01.item10;
 
+import java.util.Objects;
+
 public class ColorPoint extends Point {
     private final Color color;
 
@@ -9,18 +11,48 @@ public class ColorPoint extends Point {
     }
 
     // 대칭성 위배
+//    @Override
+//    public boolean equals(Object o) {
+//        if (!(o instanceof ColorPoint colorPoint)) {
+//            return false;
+//        }
+//        return super.equals(o) && colorPoint.color == color;
+//    }
+
+    // 추이성 위배
+//    @Override
+//    public boolean equals(Object o) {
+//        if (!(o instanceof Point)) {
+//            return false;
+//        }
+//        if (!(o instanceof ColorPoint colorPoint)) {
+//            return o.equals(this);
+//        }
+//        return super.equals(o) && colorPoint.color == color;
+//    }
+
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof ColorPoint colorPoint)) {
-            return false;
-        }
-        return super.equals(o) && colorPoint.color == color;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ColorPoint that = (ColorPoint) o;
+        return color == that.color;
     }
 
     public static void main(String[] args) {
-        Point point = new Point(1, 2);
-        ColorPoint colorPoint = new ColorPoint(1, 2, Color.RED);
-        System.out.println(point.equals(colorPoint));
-        System.out.println(colorPoint.equals(point));
+        // 대칭성 테스트
+//        Point point = new Point(1, 2);
+//        ColorPoint colorPoint = new ColorPoint(1, 2, Color.RED);
+//        System.out.println(point.equals(colorPoint));
+//        System.out.println(colorPoint.equals(point));
+
+        // 추이성 테스트
+        ColorPoint p1 = new ColorPoint(1, 2, Color.RED);
+        Point p2 = new Point(1, 2);
+        ColorPoint p3 = new ColorPoint(1, 2, Color.BLUE);
+        System.out.println(p1.equals(p2));
+        System.out.println(p2.equals(p3));
+        System.out.println(p3.equals(p1));
     }
 }
--- src/main/java/io/lcalmsky/effectivejava/chapter01/item10/Point.java
+++ src/main/java/io/lcalmsky/effectivejava/chapter01/item10/Point.java
@@ -1,10 +1,8 @@
 package io.lcalmsky.effectivejava.chapter01.item10;
 
-import java.util.Objects;
-
 public class Point {
-    private final int x;
-    private final int y;
+    protected final int x;
+    protected final int y;
 
     public Point(int x, int y) {
         this.x = x;
@@ -13,11 +11,24 @@ public class Point {
 
     @Override
     public boolean equals(Object o) {
+        System.out.println("1");
         if (!(o instanceof Point point)) {
+            System.out.println("2");
             return false;
         }
-        return point.x == x && point.y == y;
+        System.out.println("3");
+        return point.x == this.x && point.y == this.y;
     }
+
+    // 리스코프 치환법칙 위배
+//    @Override
+//    public boolean equals(Object o) {
+//        if (o == null || o.getClass() != getClass()) {
+//            return false;
+//        }
+//        Point point = (Point) o;
+//        return point.x == x && point.y == y;
+//    }
 
     //    @Override
 //    public boolean equals(Object o) {
